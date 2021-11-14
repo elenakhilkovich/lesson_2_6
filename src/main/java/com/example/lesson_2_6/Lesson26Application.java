9c396f77c5d81e2c3c3f9cf3804ce30c0d2187c4
Елена Хилькович <elenakhilkovich@gmail.com>
2021-11-14 22:24:01 +0300

    Initial commit

---
+++ src/main/java/com/example/lesson_2_6/Lesson26Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson_2_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lesson_2_6/Lesson26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson_2_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
